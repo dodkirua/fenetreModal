4f0dc6b12cd636dfa9e7a51b9e835eaccecbcf87
dodkirua <pybouttefeux@gmail.com>
2021-01-03 20:21:29 +0100

    fenetre basique

--- js/WindowModal.js
+++ js/WindowModal.js
@@ -4,35 +4,55 @@ class Window{
         this.height = height;
         this.background = background;
         this.id = id;
+        this.info;
+        this.divButton;
     }
     create(parent){
-
-
-        let shadow = document.createElement("div");
-        shadow.style.backgroundColor = "grey";
-        shadow.style.width = "100%";
-        shadow.style.height = "100%";
-        shadow.style.position = "fixed";
-        shadow.id = this.id;
-        shadow.top = 0;
-        shadow.left = 0;
-        shadow.style.zIndex = 1
-        parent.append(shadow);
-
         let div = document.createElement("div");
-        div.style.backgroundColor = this.background;
-        div.style.width = this.width+"%";
-        div.style.height = this.height+"%";
-        div.style.position = "fixed";
-        div.style.left = 50-((this.width)/2)+"%";
-        div.style.top = 50-((this.height)/2)+"%";
-        div.id = this.id;
+        if (this.background !== undefined){
+            div.style.backgroundColor = this.background;
+        }
+
+        if (this.width !== undefined){
+            div.style.width = this.width+"%";
+            div.style.left = 50-((this.width)/2)+"%";
+        }
+        if (this.height !== undefined){
+            div.style.height = this.height+"%";
+            div.style.top = 50-((this.height)/2)+"%";
+        }
+        if (this.id === undefined){
+            div.id = "modal";
+        }
+        else {
+            div.id = this.id;
+        }
         parent.append(div);
-
-
+        let info = document.createElement("div");
+        info.style.width = "100%";
+        info.style.height = "90%";
+        info.id = "info";
+        div.append(info);
+        let divButton = document.createElement("div");
+        divButton.style.width = "100%";
+        divButton.style.height = "10%";
+        divButton.id = "divButton";
+        let buttonCancel = document.createElement("button");
+        buttonCancel.innerHTML = "CANCEL";
+        let buttonValid = document.createElement("button");
+        buttonValid.innerHTML = "OK";
+        divButton.append(buttonCancel);
+        divButton.append(buttonValid)
+        div.append(divButton);
+        buttonCancel.addEventListener("click",function (){
+            div.remove();
+        })
     }
 }
 
-let Window1 = new Window(90,90,"black");
+let Window1 = new Window(50,50,"black");
+let buttonTest = document.getElementById("test");
 
-Window1.create(document.body);
+buttonTest.addEventListener("click", function (){
+        Window1.create(document.body);
+});
